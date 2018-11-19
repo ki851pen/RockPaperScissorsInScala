f80c28ba31035de5df8e91704adf70e7634f3d15
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2018-11-19 12:55:50 +0100

    TUI updated

--- src/main/scala/de/htwg/se/rockpaperscissors/aview/TUI.scala
+++ src/main/scala/de/htwg/se/rockpaperscissors/aview/TUI.scala
@@ -1,5 +1,14 @@
 package de.htwg.se.rockpaperscissors.aview
 
-class TUI {
+import de.htwg.se.rockpaperscissors.model._
 
+class TUI {
+  def processInputLine(input: String, deck:Deck):Array[Deck] = {
+    input match {
+      case "n" => new Deck()
+        (new Deck(null), new Deck(null)
+      case "p" =>
+
+    }
+  }
 }
